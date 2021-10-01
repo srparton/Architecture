be7ea325a7a68951de41e5f0376255ac14becb41
Spencer Parton <srparton1@buffs.wtamu.edu>
2021-09-30 20:34:38 -0500

    Added new constructs to be commented

--- Constructs/iPlusPlus.c
+++ Constructs/iPlusPlus.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+// #include <stdbool.h>
 int main(){
     int i = 0;
     i++;
--- Constructs/script.sh
+++ Constructs/script.sh
@@ -1,17 +1,20 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 # 
 # NEW_DIR: 
 # args[1] = file
-# Purpose: 
-# Precondition: 
-# Postcondition:
+# Purpose: Add assembly test to Assebley files after objdump is called.
+# Precondition: scrip is in folder with .c files. Folder by same name as file (without.c) exists in Assembly folder
+# Postcondition: scrip will redirect output of objdump to Assembly/FILE_NAME. 
+#
+# NOTICE: I have changed redirect from create new to append to i dont destory any comments made on files.
+#   if reused look at altering back to original ">" instead of ">>"
 FILE="$1"
-#if [-d ./Assembly/$FILE]
-#then
-    gcc -c $FILE.c
-    #gcc -Og -c Constructs/$FILE.c
-	objdump -d $FILE.o >> ../Assembly/$FILE/$FILE.txt
-#else
+# if [-d ./Assembly/$FILE]
+# then
+    # gcc -c $FILE.c
+    gcc -Og $FILE.c
+	objdump -d -S $FILE.o >> ../Assembly/$FILE/$FILE.txt
+# else
 #    echo "folder does not exist"
-#fi
+# fi
 	
